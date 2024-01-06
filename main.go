2901803664b1d60e5f64b36363e2b7781349ba12
icepie <icepie.dev@gmail.com>
2024-01-06 10:44:52 +0800

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/icepie/ga-starter/cmd"
+	"github.com/urfave/cli/v2"
+)
+
+// Usage: go build -ldflags "-X main.VERSION=x.x.x"
+var VERSION = "v1.0.0"
+
+// @title gastarter
+// @version v1.0.0
+// @description A test API service based on golang.
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @schemes http https
+// @basePath /
+func main() {
+	app := cli.NewApp()
+	app.Name = "gastarter"
+	app.Version = VERSION
+	app.Usage = "A test API service based on golang."
+	app.Commands = []*cli.Command{
+		cmd.StartCmd(),
+		cmd.StopCmd(),
+		cmd.VersionCmd(VERSION),
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
